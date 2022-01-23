d957301fcf008193c94065e28fc25a2d19dc2adf
inanimate36 <Inan.bostanci@web.de>
2022-01-23 15:18:22 +0100

    upload scripts and files

---
+++ common_variables.R
@@ -0,0 +1,16 @@
+# Common non-csv data between CV and resume
+
+# Header with description of what is trying to be accomplished with this CV/Resume
+intro_text <- "Currently, I am interning and writing my thesis at Statistics Netherlands (CBS). In this research project, traffic loop sensor data is utilised to improve models that estimate travel behavior. The thesis is supervised by Dr. Peter Lugtig and Yvonne Gootzen.
+"
+
+
+# Language skills for skill bars visualization
+skills <- tribble(
+  ~skill,               ~level,
+  "MS Office",          5,
+  "R",                  4,
+  "Python",             2.5,
+  "SPSS",               2,
+  "Stata",              2,
+)
