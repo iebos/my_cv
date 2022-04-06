f6abe28a64b9c8ff40fbb35f6e9663795e5721c3
inanimate36 <Inan.bostanci@web.de>
2022-04-06 22:00:26 +0200

    changed skill levels

--- common_variables.R
+++ common_variables.R
@@ -1,16 +1,15 @@
 # Common non-csv data between CV and resume
 
 # Header with description of what is trying to be accomplished with this CV/Resume
-intro_text <- "Currently, I am interning and writing my thesis at Statistics Netherlands (CBS). In this research project, traffic loop sensor data is utilised to improve models that estimate travel behavior. The thesis is supervised by Dr. Peter Lugtig and Yvonne Gootzen.
-"
+intro_text <- "Currently, I am interning and writing my thesis at Statistics Netherlands (CBS). In this research project, traffic loop sensor data and administrative data are utilised to improve models that estimate travel behavior and predict traffic on a nationwide level. The thesis is supervised by Dr. Peter Lugtig and Yvonne Gootzen."
 
 
 # Language skills for skill bars visualization
 skills <- tribble(
   ~skill,               ~level,
   "MS Office",          5,
-  "R",                  4,
-  "Python",             2.5,
-  "SPSS",               2,
-  "Stata",              2,
+  "R",                  4.5,
+  "Python",             4,
+  "SPSS",               5,
+  "Stata",              2.5,
 )
