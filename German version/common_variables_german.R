68f67d4282c8b244001b8bdb58f7a9df2a4f7824
inanimate36 <Inan.bostanci@web.de>
2022-07-11 17:46:35 +0200

    added german version in subfolder

---
+++ German version/common_variables_german.R
@@ -0,0 +1,23 @@
+# Common non-csv data between CV and resume
+
+# Header with description of what is trying to be accomplished with this CV/Resume
+intro_text <- "Currently, I am interning and finishing up my thesis at Statistics Netherlands (CBS). In this research project, traffic loop sensor data and administrative data are utilised to improve models that estimate travel behavior and predict traffic on a nationwide level. The thesis is supervised by Dr. Peter Lugtig and Yvonne Gootzen."
+
+
+# Language skills for skill bars visualization
+skills <- tribble(
+  ~skill,               ~level,
+  "R",                  4.5,
+  "Python",             4,
+  "SPSS",               5,
+  "Stata",              2.5,
+  "MS Office",          5,
+)
+
+language <- tribble(
+  ~skill,               ~level,
+  "Deutsch",            5,
+  "Englisch",             5,
+  "Türkisch",            2.5,
+  "Französisch",             1,
+)
